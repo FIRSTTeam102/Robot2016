7f733a96dc69bfba3df4f1d402854d44e9add68f
gearheads4 <firstteam102@gmail.com>
2016-01-25 19:24:08 -0500

    Pneumatics things

--- src/org/usfirst/frc/team102/robot/commands/SetSolenoid.java
+++ src/org/usfirst/frc/team102/robot/commands/SetSolenoid.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team102.robot.commands;
 
 import org.usfirst.frc.team102.robot.Robot;
+import org.usfirst.frc.team102.robot.subsystems.Pneumatics;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -9,14 +10,18 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class SetSolenoid extends Command {
 
+	private boolean turnOn = false;
+	
     public SetSolenoid(boolean turnOn) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.robotPneumatics);
+    	this.turnOn = turnOn;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Pneumatics.setSolenoid(turnOn);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -30,6 +35,7 @@ public class SetSolenoid extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	
     }
 
     // Called when another command which requires one or more of the same
